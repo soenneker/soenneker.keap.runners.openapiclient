e61b17dd7d0f4486f1d9584c6c5a18c41bef01ad
soenneker <jake@soenneker.com>
2026-09-03 22:01:26 -0500

    Add interface inheritdoc references

--- src/Soenneker.Keap.Runners.OpenApiClient/Utils/FileOperationsUtil.cs
+++ src/Soenneker.Keap.Runners.OpenApiClient/Utils/FileOperationsUtil.cs
@@ -21,6 +21,7 @@ using Soenneker.Utils.Yaml.Abstract;
 using System.Collections.Generic;
 namespace Soenneker.Keap.Runners.OpenApiClient.Utils;
 
+/// <inheritdoc cref="IFileOperationsUtil" />
 public sealed class FileOperationsUtil : IFileOperationsUtil
 {
     private readonly ILogger<FileOperationsUtil> _logger;
